6d33fa0dde7274fed56e66aad17b5803c2506109
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2021-08-20 11:08:39 -0300

    basic style set

--- js/index.js
+++ js/index.js
@@ -11,11 +11,11 @@ const saveButton = document.querySelector('#save-button');
 const startButton = document.querySelector('#startTimer');
 const pauseButton = document.querySelector('#pauseTimer');
 
-
+console.log(window.history)
 saveSettings();
 checkCycle();
 
 startButton.addEventListener('click', startPomodoro);
 pauseButton.addEventListener('click', pausePomodoro);
-timerForm.addEventListener('change', readInputs);
-saveButton.addEventListener('click', saveInputs);
+// timerForm.addEventListener('change', readInputs);
+// saveButton.addEventListener('click', saveInputs);
